c69d494fb3e9fd27cc5492d6f2d0e80649dbb1bc
Mrwangrz <1203976774@qq.com>
2018-11-17 20:06:42 +0800

    引入了弹出框

--- soulideaapp/src/components/newsPage/friendsList.vue
+++ soulideaapp/src/components/newsPage/friendsList.vue
@@ -21,9 +21,11 @@
 </template>
 
 <script>
+  import {MessageBox} from "mint-ui";
+  import axios from "axios";
+
   export default {
-    components:{
-    },
+    components: {},
     data() {
       return {
         startX: 0,   //触摸位置
@@ -31,7 +33,7 @@
         moveX: 0,   //滑动时的位置
         disX: 0,    //移动距离
         deleteSlider: '',//滑动时的效果,使用v-bind:style="deleteSlider",
-        options:{
+        options: {
           btn: {
             text: '按钮',
             style: {
@@ -64,68 +66,87 @@
          }
        },*/
       handleDelete() {
-        },
-  touchStart(ev)
-  {
-    ev = ev || event
-    //tounches类数组，等于1时表示此时有只有一只手指在触摸屏幕
+        MessageBox.confirm("确认删除好友吗",{
+          title: '  ',
+          message: '确定执行此操作?',
+          showCancelButton: true
+        }).then(
+          action => {
+            axios({
+              method: "post",
+              url: "/Soulidea/friend/deletefriend",
+              headers: {
+                'Content-type': 'application/x-www-form-urlencoded'
+              },
+              data: {
+                id: id
+              }
+            }).then(
+              (data) => {
+                console.log(data);
+              }
+            ).catch(()=>{});
+          }
+        ).catch(()=>{});
+      },
+      touchStart(ev) {
+        ev = ev || event;
+        //tounches类数组，等于1时表示此时有只有一只手指在触摸屏幕
 
-    if (ev.touches.length == 1) {
-      // 记录开始位置
-      this.startX = ev.touches[0].clientX;
-    }
-  }
-  ,
-  touchMove(ev)
-  {
-    ev = ev || event;
-    //获取删除按钮的宽度，此宽度为滑块左滑的最大距离
-    let wd = 105;
-    let wdRem = 2.1;
-    if (ev.touches.length == 1) {
-      // 滑动时距离浏览器左侧实时距离
-      this.moveX = ev.touches[0].clientX
+        if (ev.touches.length == 1) {
+          // 记录开始位置
+          this.startX = ev.touches[0].clientX;
+        }
+      }
+      ,
+      touchMove(ev) {
+        ev = ev || event;
+        //获取删除按钮的宽度，此宽度为滑块左滑的最大距离
+        let wd = 105;
+        let wdRem = 2.1;
+        if (ev.touches.length == 1) {
+          // 滑动时距离浏览器左侧实时距离
+          this.moveX = ev.touches[0].clientX
 
-      //起始位置减去 实时的滑动的距离，得到手指实时偏移距离
-      this.disX = this.startX - this.moveX;
-      // 如果是向右滑动或者不滑动，不改变滑块的位置
-      if (this.disX < 0 || this.disX == 0) {
-        this.deleteSlider = "transform:translateX(0px)";
-        // 大于0，表示左滑了，此时滑块开始滑动
-      } else if (this.disX > 0) {
-        //具体滑动距离我取的是 手指偏移距离*5。
-        this.deleteSlider = "transform:translateX(-" + this.disX * 5 + "px)";
+          //起始位置减去 实时的滑动的距离，得到手指实时偏移距离
+          this.disX = this.startX - this.moveX;
+          // 如果是向右滑动或者不滑动，不改变滑块的位置
+          if (this.disX < 0 || this.disX == 0) {
+            this.deleteSlider = "transform:translateX(0px)";
+            // 大于0，表示左滑了，此时滑块开始滑动
+          } else if (this.disX > 0) {
+            //具体滑动距离我取的是 手指偏移距离*5。
+            this.deleteSlider = "transform:translateX(-" + this.disX * 5 + "px)";
 
-        // 最大也只能等于删除按钮宽度
-        if (this.disX * 5 >= wd) {
-          this.deleteSlider = "transform:translateX(-" + wdRem + "rem)";
+            // 最大也只能等于删除按钮宽度
+            if (this.disX * 5 >= wd) {
+              this.deleteSlider = "transform:translateX(-" + wdRem + "rem)";
 
+            }
+          }
+        }
+      }
+      ,
+      touchEnd(ev) {
+        ev = ev || event;
+        let wd = 105;
+        let wdRem = 2.1;
+        if (ev.changedTouches.length == 1) {
+          let endX = ev.changedTouches[0].clientX;
+
+          this.disX = this.startX - endX;
+          //如果距离小于删除按钮一半,强行回到起点
+
+          if ((this.disX * 5) < (wd / 2)) {
+
+            this.deleteSlider = "transform:translateX(0px)";
+          } else {
+            //大于一半 滑动到最大值
+            this.deleteSlider = "transform:translateX(-" + wdRem + "rem)";
+          }
         }
       }
     }
-  }
-  ,
-  touchEnd(ev)
-  {
-    ev = ev || event;
-    let wd = 105;
-    let wdRem = 2.1;
-    if (ev.changedTouches.length == 1) {
-      let endX = ev.changedTouches[0].clientX;
-
-      this.disX = this.startX - endX;
-      //如果距离小于删除按钮一半,强行回到起点
-
-      if ((this.disX * 5) < (wd / 2)) {
-
-        this.deleteSlider = "transform:translateX(0px)";
-      } else {
-        //大于一半 滑动到最大值
-        this.deleteSlider = "transform:translateX(-" + wdRem + "rem)";
-      }
-    }
-  }
-  }
   }
 </script>
 <style scoped>
@@ -210,4 +231,6 @@
     font-size: 12px;
     color: #999;
   }
+
+
 </style>
--- soulideaapp/src/components/newsPage/searchUserByName/searchUserByNameList.vue
+++ soulideaapp/src/components/newsPage/searchUserByName/searchUserByNameList.vue
@@ -1,16 +1,47 @@
 <template>
   <div class="all">
-    <img src="/static/image/newsPage/messagePage/logo.png" alt="">
     <div class="info">
-      <p>用户名</p>
+      <img src="/static/image/newsPage/messagePage/logo.png" alt="">
+      <span>
+         <p>用户名</p>
       <p>个签</p>
+      </span>
+    </div>
+
+    <div class="add" @click="handleAddFriend()">
+      加为好友
     </div>
   </div>
+
 </template>
 
 <script>
+  import {MessageBox} from "mint-ui";
     export default {
-        name: "search-user-by-name"
+        name: "search-user-by-name",
+        methods:{
+          //处理添加好友
+              handleAddFriend(){
+                MessageBox.confirm("添加对方为好友",{
+                  title:" "
+                }).then(
+                  action=>{
+                    axios({
+                      methods:"get",
+                      url:"",
+                      data:{
+                      }
+                    }).then(
+                      (data)=>{
+                      /*MessageBox.alert("添加成功",{
+                          title:" "
+                        });*/
+                      }
+                    ).catch(()=>{});
+                  }
+                ).catch(()=>{});
+              }
+        }
     }
 </script>
 
@@ -20,7 +51,7 @@
     width: 100%;
     height: 1.6rem;
     margin-top: .1rem;
-    padding: 0 .6rem;
+    padding: 0 .4rem;
     display: flex;
     align-items: center;
     border:1px solid #efefef;
@@ -30,19 +61,22 @@
   img{
     width: 1.1rem;
     height: 1.1rem;
-
+    margin-right: .2rem;
   }
   .info{
     display: flex;
     align-items: center;
+    justify-content: space-between;
+  }
+  span{
+    display: flex;
     flex-direction: column;
     justify-content: space-between;
-    height: .8rem;
-    margin-left: .2rem;
+    height:.9rem;
   }
   p{
     text-align: left;
-    width: 4rem;
+    width: 3.5rem;
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
@@ -54,6 +88,16 @@
   }
   p:last-of-type{
     color: #999;
+  }
+  .add{
+    border: 1px solid #999999;
+    height: .6rem;
+    width: 3rem;
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #666;
   }
 
 </style>
